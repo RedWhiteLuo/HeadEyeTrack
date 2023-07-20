6298c2ace838da4de40b97dd44f6382764cbcbc9
RedWhiteLuo <prof.redwhite@gmail.com>
2023-07-20 17:09:04 +0800

    change

---
+++ Module_Kalman.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+
+class Kalman:
+    def __init__(self):
+        self.kalman = cv2.KalmanFilter(4, 2)  # 四个输入，需要预测两个
+        self.kalman.measurementMatrix = np.array([[1, 0, 0, 0],
+                                                  [0, 1, 0, 0]], np.float32)
+        self.kalman.transitionMatrix = np.array([[1, 0, 1, 0],
+                                                 [0, 1, 0, 1],
+                                                 [0, 0, 1, 0],
+                                                 [0, 0, 0, 1]], np.float32)  # 传递矩阵
+        self.kalman.processNoiseCov = np.array([[1, 0, 0, 0],
+                                                [0, 1, 0, 0],
+                                                [0, 0, 1, 0],
+                                                [0, 0, 0, 1]], np.float32) * 0.0  # 噪声
+        self.last_measurement = None
+        self.last_prediction = None
+
+    def Position_Predict(self, x, y):
+        """
+        :param x: 输入 x 坐标
+        :param y: 输入 y 坐标
+        :return:  返回预测的坐标 x, y 无打包
+        """
+        # 设置为全局变量
+        measurement = np.array([[x], [y]], np.float32)
+        # 第一次实际测量
+        if self.last_measurement is None:
+            self.kalman.statePre = np.array([[x], [y], [0], [0]], np.float32)
+            self.kalman.statePost = np.array([[x], [y], [0], [0]], np.float32)
+            prediction = measurement
+        # 不是第一次则进行预测
+        else:
+            self.kalman.correct(measurement)
+            prediction = self.kalman.predict()
+        # 进行迭代
+        self.last_prediction = prediction.copy()
+        self.last_measurement = measurement
+        return prediction[:2][0], prediction[:2][1]
+
+    def clean(self):
+        self.last_measurement = None
+
+
+"""
+https://blog.csdn.net/weixin_55737425/article/details/124560990
+"""
--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
-import time
 import cv2
+import time
 from Skps.core.api.facer import FaceAna
-from head_pos import get_pose
+from head_pos import calculate_face_vector
 
 facer = FaceAna()
 vide_capture = cv2.VideoCapture(0)
@@ -10,49 +10,31 @@ while True:
     ret, image = vide_capture.read()
     image = cv2.flip(image, 1)
     if ret:
-        img_show = image.copy()
-        
-        star = time.time()
+        star = time.perf_counter()
         result = facer.run(image)
-        duration = time.time() - star
-        
-        fps = 1 / duration if duration else 0
-        cv2.putText(img_show, "FPS: " + "{:7.2f}".format(fps), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0))
+        duration = time.perf_counter() - star
 
+        fps = 1 / duration if duration else 0
+        cv2.putText(image, "FPS: " + "{:7.2f}".format(fps), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0))
+        t1 = time.perf_counter()
         for face_index in range(len(result)):
 
             face_kp, face_kp_score, index = result[face_index]['kps'], result[face_index]['scores'], 0
+
+            vector_p1, vector_p2, vector = calculate_face_vector(face_kp)
+            cv2.line(image, vector_p1, vector_p2, (0, 0, 255), 3)
+            cv2.putText(image, str(vector), (20, 460), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+
             for landmarks_index in range(face_kp.shape[0]):
-                index += 1
+                kp_score = face_kp_score[landmarks_index]
                 kp_coords = face_kp[landmarks_index]
                 kp_coords = [int(kp_coords[0]), int(kp_coords[1])]
-                if index == 61:
-                    eyeleft, eyeright, eyetop, eyebottom = kp_coords[0], kp_coords[0], kp_coords[1], kp_coords[1]
-                if 60 < index < 77:
-                    eyeleft = kp_coords[0] if kp_coords[0] < eyeleft else eyeleft
-                    eyeright = kp_coords[0] if kp_coords[0] > eyeright else eyeright
-                    eyetop = kp_coords[1] if kp_coords[1] < eyetop else eyetop
-                    eyebottom = kp_coords[1] if kp_coords[1] > eyebottom else eyebottom
-                if index == 77:
-                    eye_img = image[eyetop - 5:eyebottom + 5, eyeleft - 10:eyeright + 10]
-                    cv2.namedWindow('eyes', cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
-                    cv2.imshow("eyes", eye_img)
-                    
-                    p1, p2, vector = get_pose(face_kp)
-                    cv2.line(img_show, p1, p2, (0, 0, 255), 3)
-                    cv2.putText(img_show, str(vector), (20, 460), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                                (0, 0, 255), thickness=2)
-                cv2.putText(img_show, str(index), (int(kp_coords[0]), int(kp_coords[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
-                            (255, 255, 255),
-                            thickness=1)
-                score = face_kp_score[landmarks_index]
-                # color = (255, 255, 255)
-                if score > 0.85:
-                    color = (255, 255, 255)
-                else:
-                    color = (0, 0, int(255 * score))
-                cv2.circle(img_show, (int(kp_coords[0]), int(kp_coords[1])),
-                           color=color, radius=1, thickness=2)
-        cv2.imshow("capture", img_show)
 
-        key = cv2.waitKey(1)
+                # color = (255, 255, 255)
+                color = (0, 255, 0) if kp_score > 0.85 else (0, 0, int(255 * kp_score))
+                cv2.circle(image, (int(kp_coords[0]), int(kp_coords[1])), color=color, radius=2, thickness=1)
+
+        cv2.imshow("capture", image)
+        cv2.waitKey(1)
+        end = time.perf_counter()
+        print(end - t1)
